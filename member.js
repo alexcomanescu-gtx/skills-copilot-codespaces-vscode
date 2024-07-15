e73c7e74730346ddcce726dab12a7bb78fd21349
alexcomanescu-gtx <alex.comanescu@garrettmotion.com>
2024-07-15 16:36:57 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+    var member = {
+        name: "John Doe",
+        age: 26,
+        skills: ["JavaScript", "React", "Node", "MongoDB"],
+        address: {
+            city: "California",
+            country: "USA"
+        }
+    };
+    return member;
+}
